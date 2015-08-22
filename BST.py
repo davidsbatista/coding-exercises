0b1ed108ad7a89563f50ee369b07efffc6d869b6
davidsbatista <dsbatista@gmail.com>
2015-08-22 17:03:11 +0100

    added BST exercices

--- BST.py
+++ BST.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
 
 __author__ = "David S. Batista"
 __email__ = "dsbatista@inesc-id.pt"
@@ -34,27 +35,6 @@ class Node:
         if self.right_child is not None:
             self.right_child.in_order_tree_walk()
 
-    @staticmethod
-    def max(node):
-        while node.right_child is not None:
-            node = node.right_child
-        return node.value
-
-    @staticmethod
-    def min(node):
-        while node.left_child is not None:
-            node = node.left_child
-        return node.value
-
-    #TODO: bugged
-    def is_valid_bst(self, min, max):
-        if (min < self.value < max and
-            self.is_valid_bst(self.left_child, min, self.value) and
-            self.is_valid_bst(self.right_child, max, self.value)):
-            return True
-        else:
-            return False
-
     #TODO: delete
 
     #TODO: return Node where value occurs
@@ -68,6 +48,18 @@ class Node:
             self.right_child.search(k)
 
 
+def max_node(node):
+    while node.right_child is not None:
+        node = node.right_child
+    return node.value
+
+
+def min_node(node):
+    while node.left_child is not None:
+        node = node.left_child
+    return node.value
+
+
 # Pre-order or deep-first-search
 def print_dfs(node):
     if node is not None:
@@ -76,7 +68,7 @@ def print_dfs(node):
         print_dfs(node.right_child)
 
 
-# in-order of the elements in the b-tree
+# in-order
 def in_order_tree_walk(node):
     if node.left_child is not None:
         in_order_tree_walk(node.left_child)
@@ -94,7 +86,7 @@ def post_order(node):
 
 
 # https://www.cs.bu.edu/teaching/c/tree/breadth-first/
-#TODO: print a newline at each level
+# using a queue (FIFO)
 def print_bfs(node):
     l = [node]
     while len(l) > 0:
@@ -106,49 +98,120 @@ def print_bfs(node):
             l.insert(0, v.right_child)
 
 
+#TODO: print a newline at each level
+def print_bfs_level(node):
+    l = [node]
+    while len(l) > 0:
+        v = l.pop()
+        print v.value,
+        if v.left_child is not None:
+            l.insert(0, v.left_child)
+        if v.right_child is not None:
+            l.insert(0, v.right_child)
+
+
+def is_valid_bst(node, stack):
+    """
+    The left subtree of a node contains only nodes with keys less than the node’s key.
+    The right subtree of a node contains only nodes with keys greater than the node’s key.
+    Both the left and right subtrees must also be binary search trees.
+
+    Method 1:
+        - in-order transversal and check in-line if array is sorted
+    """
+    if node.left_child is not None:
+        is_valid_bst(node.left_child, stack)
+    stack.append(node.value)
+    if node.right_child is not None:
+        is_valid_bst(node.right_child, stack)
+    return stack
+
+
+def is_valid_bst2(node, previous):
+    #TODO:
+    """
+    While doing In-Order traversal, we can keep track of previously visited node.
+    If the value of the currently visited node is less than the previous value,
+    then tree is not BST.
+    """
+    pass
+
+
+def find_path(node, k, path):
+    if k == node.value:
+        return path
+    elif k < node.value:
+        path.append(node.value)
+        find_path(node.left_child, k, path)
+    else:
+        path.append(node.value)
+        find_path(node.right_child, k, path)
+
+
+def lowest_common_ancestor(root, n1, n2):
+    """
+    Method 1 (By Storing root to n1 and root to n2 paths):
+    Following is simple O(n) algorithm to find LCA of n1 and n2.
+    1) Find path from root to n1 and store it in a vector or array.
+    2) Find path from root to n2 and store it in another vector or array.
+    3) Traverse both paths till the values in arrays are same. Return the common element just before the mismatch.
+    """
+    p1 = []
+    p2 = []
+    find_path(root, n1, p1)
+    find_path(root, n2, p2)
+    print n1, p1
+    print n2, p2
+    x = -1
+    y = -1
+    #TODO: check if n1 in p2 or if n2 in p1
+    while x == y and len(p1) > 0 and len(p2) > 0:
+        x = p1.pop(0)
+        y = p2.pop(0)
+        lca = x
+    print lca
+
+
 def main():
     arr = [3, 1, 6, 4, 7, 10, 14, 13]
     root = Node(8)
     for i in arr:
         root.insert(i)
 
-    #print root.is_valid_bst(sys.maxint, -sys.maxint - 1)
+    lowest_common_ancestor(root, 4, 3)
+
     """
-    print "ordered elements: "
-    print "\n"
-    x = 13
-    node = root.search(x)
-    print node
-    print "max:", root.max(root)
-    print "min:", root.min(root)
-    """
-    """
-    preorder: visit each node before its children.
-    postorder: visit each node after its children.
-    inorder (for binary trees only): visit left subtree, node, right subtree.
+    print "\nmin:", min_node(root)
+    print "\nmax:", max_node(root)
+
+    stack = is_valid_bst(root, [])
+    for x in range(1, len(stack)):
+        if stack[x-1] > stack[x]:
+            print False
+            break
+    print "\nis valid BST:", stack
     """
 
-    print "in order:"
+    #is_valid_bst2(root, -1)
+
+    """
+    # inorder (for binary trees only): visit left subtree, node, right subtree.
+    print "\nInorder Traversal:"
     in_order_tree_walk(root)
-    print "\n\ndepth-first (pre-order):"
+
+    #preorder: visit each node before its children.
+    print "\n\nPreorder Traversal (Depth-first):"
     print_dfs(root)
-    print "\n\npost-order:"
+
+    #postorder: visit each node after its children.
+    print "\n\nPostorder Traversal:"
     post_order(root)
+
     print '\n\nBreadth-First Traversal'
     print_bfs(root)
 
+    #node = root.search(x)
     """
-    print 'Breadth-First Traversal'
-    tree.bft()
-    print 'Inorder Traversal'
-    tree.inorder(tree.root)
-    print 'Preorder Traversal'
-    tree.preorder(tree.root)
-    print 'Postorder Traversal'
-    tree.postorder(tree.root)
-
-    """
-
 
 if __name__ == "__main__":
     main()
