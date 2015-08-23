c1f1418a1ae955bd110b76a02e58bfbc0db87b7f
davidsbatista <dsbatista@gmail.com>
2015-08-24 00:14:23 +0100

    added

---
+++ heaps.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "David S. Batista"
+__email__ = "dsbatista@inesc-id.pt"
+
+"""
+- Find the minimum element in a stack in O(1) time
+http://stackoverflow.com/questions/685060/design-a-stack-such-that-getminimum-should-be-o1
+"""
+
+
+def min_queue():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
